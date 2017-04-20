b84c314e737cc6330b095a38438f2363215f7b81
Sean Yu <yusean1792@gmail.com>
2017-04-19 22:16:04 -0700

    Circles will die when hit enough, cheer, damaged sounds added

--- temp.js
+++ temp.js
@@ -1,5 +1,8 @@
-var keysPressed = {}, distPerIteration = 3, maxValueLeft = $(window).width();
+var keysPressed = {}, distPerIteration = 5, maxValueLeft = $(window).width();
 var myVar, maxValueTop = $(window).height();
+var playerWidth = $('#player').width(), playerHeight = $('#player').height();
+var minionHp = [];
+var minions = [];
 
 $(window).keydown(function(event) { keysPressed[event.which] = true });
 $(window).keyup(function(event) { keysPressed[event.which] = false});
@@ -12,10 +15,19 @@ function calcNewValue(oldValue, keyCode1, keyCode2, maxValue) {
     return newValue < 0 ? 0 : newValue > maxValue ? maxValue : newValue
 }
 
-function minion(){
+function Minion(){
     this.hp = 2;
     this.alive = true;
 }
+
+function findCenter(element){
+    var height = element.height();
+    var width = element.width();
+    var top = parseInt(element.css("top"));
+    var left = parseInt(element.css("left"));
+    return [left + width/2, top + height/2];
+}
+
 
 function spawnMinions(number){
     for(i = 0; i < number; i ++) {
@@ -29,29 +41,58 @@ function spawnMinions(number){
         div.style.top = String(tp) +'px';
         div.setAttribute("id", "minion" + String(i));
         document.body.appendChild(div);
+
+        minionHp.push(6);
+        minions.push(div);
     }
 }
 
 function beginGame(){
-    spawnMinions(3);
+    var numOfMinions = 6;
+    spawnMinions(6);
+
     var y = $('#player');
+    playerWidth = y.width(), playerHeight = y.height();
+
     $(".hidden").css("visibility", "visible");
     y.css("visibility", "visible");
-    var attackAudio = new Audio('music/attack.mp3');
-    myVar = setInterval(function(){
-        y.css({
-            left: function(index, oldValue) { return calcNewValue(oldValue, 37, 39, maxValueLeft); },
-            top: function(index, oldValue) { return calcNewValue(oldValue, 38, 40, maxValueTop); } 
-            
-        });
 
+    var attackAudio = new Audio('music/attack.mp3');
+    var damageAudio = new Audio('music/damaged.mp3');
+    var cheerAudio = new Audio('music/cheer.mp3')
+    myVar = setInterval(function(){
+        var center = findCenter($('#player'));
+        y.css({
+            left: function(index, oldValue) {
+                return calcNewValue(oldValue, 37, 39, maxValueLeft - playerWidth); 
+            },
+            top: function(index, oldValue) { 
+                return calcNewValue(oldValue, 38, 40, maxValueTop - playerHeight); 
+            } 
+        });
         if(keysPressed[32]){
+            
             attackAudio.play();
+            for (i = 0; i < minions.length; i++) {
+                var dist = playerWidth + parseInt(minions[i].offsetWidth);
+                var minionCenter = findCenter($('#minion' + i));
+                if(distance(minionCenter, center) < dist){
+                    minionHp[i] -= 1;
+                    damageAudio.play();
+                    if(minionHp[i] == 0){
+                        $('#minion' + i).css("visibility", "hidden");
+                        cheerAudio.play();
+                    }
+                }
+            }
         }
-    }, 20);
+    }, 30);
     var audio = new Audio('music/adventure_awaits.mp3');
-    audio.play();
+    //audio.play();
     document.getElementById("start_box").style.display = 'none';
 }
 
-
+function distance(c1, c2){
+    return Math.sqrt((c1[0] - c2[0]) * (c1[0] - c2[0]) + 
+            (c1[1] - c2[1]) * (c1[1] - c2[1]));
+}
