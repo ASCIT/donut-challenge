8cf610a6c5bab3616148368506ac2baa07d3f036
Sean Yu <yusean1792@gmail.com>
2017-04-21 15:26:27 -0700

    Minions now move and there is a defeat condition

--- temp.js
+++ temp.js
@@ -1,8 +1,7 @@
 var keysPressed = {}, distPerIteration = 5, maxValueLeft = $(window).width();
-var myVar, maxValueTop = $(window).height();
+var refreshId, maxValueTop = $(window).height();
 var playerWidth = $('#player').width(), playerHeight = $('#player').height();
-var minionHp = [];
-var minions = [];
+var minionHp = [], minions = [], minionXSpeeds = [], minionYSpeeds = [];
 
 $(window).keydown(function(event) { keysPressed[event.which] = true });
 $(window).keyup(function(event) { keysPressed[event.which] = false});
@@ -13,11 +12,6 @@ function calcNewValue(oldValue, keyCode1, keyCode2, maxValue) {
                    - (keysPressed[keyCode1] ? distPerIteration : 0)
                    + (keysPressed[keyCode2] ? distPerIteration : 0);
     return newValue < 0 ? 0 : newValue > maxValue ? maxValue : newValue
-}
-
-function Minion(){
-    this.hp = 2;
-    this.alive = true;
 }
 
 function findCenter(element){
@@ -44,15 +38,18 @@ function spawnMinions(number){
 
         minionHp.push(6);
         minions.push(div);
+        minionXSpeeds.push(4);
+        minionYSpeeds.push(4);
     }
 }
 
 function beginGame(){
     var numOfMinions = 6;
-    spawnMinions(6);
+    spawnMinions(numOfMinions);
 
     var y = $('#player');
     playerWidth = y.width(), playerHeight = y.height();
+    var minionWidth = minions[0].offsetWidth;
 
     $(".hidden").css("visibility", "visible");
     y.css("visibility", "visible");
@@ -60,7 +57,8 @@ function beginGame(){
     var attackAudio = new Audio('music/attack.mp3');
     var damageAudio = new Audio('music/damaged.mp3');
     var cheerAudio = new Audio('music/cheer.mp3')
-    myVar = setInterval(function(){
+    var gameOverAudio = new Audio('music/respectfully_resigned.wav');
+    refreshId = setInterval(function(){
         var center = findCenter($('#player'));
         y.css({
             left: function(index, oldValue) {
@@ -71,14 +69,13 @@ function beginGame(){
             } 
         });
         if(keysPressed[32]){
-            
             attackAudio.play();
             for (i = 0; i < minions.length; i++) {
-                var dist = playerWidth + parseInt(minions[i].offsetWidth);
+                var attackDist = playerWidth + minionWidth;
                 var minionCenter = findCenter($('#minion' + i));
-                if(distance(minionCenter, center) < dist){
+                if(distance(minionCenter, center) < attackDist){
                     minionHp[i] -= 1;
-                    damageAudio.play();
+                    if(minionHp[i] >= 0) damageAudio.play();
                     if(minionHp[i] == 0){
                         $('#minion' + i).css("visibility", "hidden");
                         cheerAudio.play();
@@ -86,10 +83,45 @@ function beginGame(){
                 }
             }
         }
+        var touchDist = (playerWidth + minionWidth)/2;
+        for (i = 0; i < minions.length; i ++) {
+            var minionCenter = findCenter($('#minion' + i));
+            if(distance(minionCenter, center) < touchDist && minionHp[i] > 0){
+                $('.game_over').css("visibility", "visible")
+                audio.pause();
+                gameOverAudio.play(); 
+                clearViews();
+                clearInterval(refreshId);
+            }
+            var newX = minionCenter[0] + minionXSpeeds[i];
+            if (newX > maxValueLeft - minionWidth/2) {
+                newX = maxValueLeft - minionWidth/2;
+                minionXSpeeds[i] = -minionXSpeeds[i];
+            }else if (newX < minionWidth/2) {
+                newX = minionWidth/2;
+                minionXSpeeds[i] = -minionXSpeeds[i];
+            }
+
+            var newY = minionCenter[1] + minionYSpeeds[i];
+            if (newY > maxValueTop - minionWidth/2) {
+                newY = maxValueTop - minionWidth/2;
+                minionYSpeeds[i] = -minionYSpeeds[i];
+            }else if (newY < minionWidth/2) {
+                newY = minionWidth/2;
+                minionYSpeeds[i] =- minionYSpeeds[i];
+            }
+            minions[i].style.left = String(newX - minionWidth/2) + 'px';
+            minions[i].style.top = String(newY - minionWidth/2) + 'px';
+        }
     }, 30);
     var audio = new Audio('music/adventure_awaits.mp3');
-    //audio.play();
+    audio.play();
     document.getElementById("start_box").style.display = 'none';
+}
+
+function clearViews() {
+    $('.minion').css("visibility","hidden");
+    $('.hidden').css("visibility","hidden");
 }
 
 function distance(c1, c2){
